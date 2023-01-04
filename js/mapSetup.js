3cfaec98d664fe23dd61ca211b51658b16221145
Arqueha <fran@arqueha.com>
2023-01-04 14:20:18 +0100

    new

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -42,7 +42,7 @@ var m = L.map("map", {
     }
 
     var mq = L.tileLayer(url, optionsObject);
-    var terrain = L.tileLayer('http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.jpg', {
+    var terrain = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     })
     mq.addTo(m);
