73f2881c405bc302ee2c50e6b44b060cc468b57b
Arqueha <fran@arqueha.com>
2023-01-04 14:59:12 +0100

    new

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -42,12 +42,12 @@ var optionsObject ={
 }
 
 var mq = L.tileLayer(url, optionsObject);
-var terrany = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+var toner = L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 })
 mq.addTo(m);
 var lc = L.control.layers({
-    "Toner": terrany,
+    "Toner": toner,
     "penstreet": mq
 }).addTo(m);
 //make the map
