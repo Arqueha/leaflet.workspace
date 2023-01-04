165ea3c2f62b629fc75ecd0cd98e03697fe37a8e
Arqueha <121859144+Arqueha@users.noreply.github.com>
2023-01-04 14:17:51 +0100

    Update mapSetup.js

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -42,9 +42,9 @@ var m = L.map("map", {
     }
 
     var mq = L.tileLayer(url, optionsObject);
-     var terrain = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-})
+    var terrain = L.tileLayer('http://{s}.tile.stamen.com/terrain/{z}/{x}/{y}.jpg', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+    })
     mq.addTo(m);
     var lc = L.control.layers({
         "Stamen terrain": terrain,
